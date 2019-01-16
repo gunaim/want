58fbf51a8bf6df21f58ab15d9e2403e3b20276c5
GunaySeyidahmed <gunay.seyidehmedqizi@gmail.com>
2019-01-15 23:23:37 -0800

    f commmit

--- build/frq-templates/js/all.js
+++ build/frq-templates/js/all.js
@@ -61,28 +61,28 @@ $( document ).ready(function() {
 
 
 
-    var f=(function() {
-        var $sidebar = $(".personal__account"),
-            $window = $(window),
-            $offset = $sidebar.offset();
-
-        var width = $sidebar.outerWidth();
-        console.log(window.innerWidth > 767);
-        console.log($window.scrollTop() > $offset.top);
-        if (window.innerWidth > 767 && $sidebar.length > 0) {
-            $window.on('scroll',function () {
-                    if ($window.scrollTop() > $offset.top) {
-                        $sidebar.css({'position': 'fixed', 'top': 90, 'width': width})
-                    }
-                    else {
-                        $sidebar.css({'position': 'static', 'width': '100%'});
-                    }
-                }
-            );
-        }
-    });
-    f();
-    $( window ).resize(f());
+    // var f=(function() {
+    //     var $sidebar = $(".personal__account"),
+    //         $window = $(window),
+    //         $offset = $sidebar.offset();
+    //
+    //     var width = $sidebar.outerWidth();
+    //     console.log(window.innerWidth > 767);
+    //     console.log($window.scrollTop() > $offset.top);
+    //     if (window.innerWidth > 767 && $sidebar.length > 0) {
+    //         $window.on('scroll',function () {
+    //                 if ($window.scrollTop() > $offset.top) {
+    //                     $sidebar.css({'position': 'fixed', 'top': 90, 'width': width})
+    //                 }
+    //                 else {
+    //                     $sidebar.css({'position': 'static', 'width': '100%'});
+    //                 }
+    //             }
+    //         );
+    //     }
+    // });
+    // f();
+    // $( window ).resize(f());
 
     // var stickyFunc;
     // (stickyFunc=function() {
@@ -104,28 +104,28 @@ $( document ).ready(function() {
     //         });
     //     }
 
-    var f1=(function() {
-        var $sidebar = $(".question__side"),
-            $window = $(window),
-            $offset = $sidebar.offset();
-
-        var width = $sidebar.outerWidth();
-        console.log(window.innerWidth > 767);
-        console.log($window.scrollTop() > $offset.top);
-        if (window.innerWidth > 767 && $sidebar.length > 0) {
-            $window.on('scroll',function () {
-                    if ($window.scrollTop() > $offset.top) {
-                        $sidebar.css({'position': 'fixed', 'top': 90, 'width': width})
-                    }
-                    else {
-                        $sidebar.css({'position': 'static', 'width': '100%'});
-                    }
-                }
-            );
-        }
-    });
-    f1();
-    $( window ).resize(f1());
+    // var f1=(function() {
+    //     var $sidebar = $(".question__side"),
+    //         $window = $(window),
+    //         $offset = $sidebar.offset();
+    //
+    //     var width = $sidebar.outerWidth();
+    //     console.log(window.innerWidth > 767);
+    //     console.log($window.scrollTop() > $offset.top);
+    //     if (window.innerWidth > 767 && $sidebar.length > 0) {
+    //         $window.on('scroll',function () {
+    //                 if ($window.scrollTop() > $offset.top) {
+    //                     $sidebar.css({'position': 'fixed', 'top': 90, 'width': width})
+    //                 }
+    //                 else {
+    //                     $sidebar.css({'position': 'static', 'width': '100%'});
+    //                 }
+    //             }
+    //         );
+    //     }
+    // });
+    // f1();
+    // $( window ).resize(f1());
 
     /*user settings*/
 
